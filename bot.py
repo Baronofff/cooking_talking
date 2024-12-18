785b93c9d360ab9d96a7fe53a13bbfac335b90b6
Arina Savina <arinasavina330@gmail.com>
2024-12-18 09:47:52 +0300

    Adding docstrings, some corrections

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ with open("token.txt", "r", encoding="utf-8") as f:
     token = f.read().strip()
 bot = telebot.TeleBot(token)
 
+#Подгружение данных, необходимых для модели
 with open('context.txt', 'r', encoding='utf-8') as file:
     CONTEXT = file.read()
 
@@ -24,21 +25,33 @@ try:
 except Exception as e:
     logging.error(f"Ошибка подключения к Redis: {e}")
 
-# Режимы работы бота
+# Определеение режимов работы бота
 MODE_RECIPES = 'recipes'
 MODE_QUESTIONS = 'questions'
 
 # Переменная для хранения текущего режима
 current_mode = MODE_RECIPES
 
-
+#Подгрузка данных json (результат парсинга)
 with open("data/recipes_data.json", "r", encoding="UTF-8") as file:
     all_recipes = json.load(file)
 user_states = {}
 headers = all_recipes[0]
 
 
-def get_recipes_by_ingredients(user_input):
+def get_recipes_by_ingredients(user_input: str) -> list:
+    """
+    Retrieves recipes that can be made with the specified ingredients.
+
+    Args:
+        user_input (str): User input of ingredients
+        (separated by commas)
+
+    Returns:
+        list: List of recipes with given ingredients or
+        an explanation message (there are no recipes with such ingredients)
+
+    """
     result = []
     ingredients = set(user_input.lower().split(", "))
 
@@ -62,7 +75,17 @@ def get_recipes_by_ingredients(user_input):
     return result
 
 
-def send_recipes_page(chat_id):
+def send_recipes_page(chat_id: int) -> None:
+    """
+    Sends a page of recipes to the user in a Telegram chat.
+
+    Args:
+        chat_id (int): The user id in Telegram chat
+
+    Returns:
+        None. It directly sends message to the user.
+
+    """
     user_state = user_states[chat_id]
     recipes = user_state["recipes"]
     page = user_state["page"]
@@ -91,6 +114,13 @@ def send_recipes_page(chat_id):
 
 
 def create_category_keyboard():
+    """
+     Creates a keyboard for selecting recipe categories in a Telegram bot.
+
+    Returns:
+        telebot.types.ReplyKeyboardMarkup: A keyboard markup object
+        with recipe categories.
+    """
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(
         "Горячее",
@@ -107,7 +137,18 @@ def create_category_keyboard():
     return markup
 
 
-def handle_category(message):
+def handle_category(message: telebot.types.Message) -> None:
+    """
+    Handles the user's selection of a recipe category.
+
+    Args:
+        message (telebot.types.Message): The message object containing the user's
+        selected category.
+
+    Returns:
+        None. This function will either send recipes for a specific category
+         or a message indicating that there are no recipes available.
+    """
     category = message.text
     recipe_list = []
     cat_index = headers.index("Категория")
@@ -131,7 +172,18 @@ def handle_category(message):
 
 
 @bot.message_handler(commands=["start"])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message) -> None:
+    """
+    Sends a welcome message to the user when they start the bot.
+
+    Args:
+        message (telebot.types.Message): The message with information
+        about the user's input (it also contains the chat id).
+
+    Returns:
+        None. It sends a message to the user and displays a keyboard
+         for category selection.
+    """
     bot.send_message(
         message.chat.id,
         "Привет! Введи ингредиенты, или выбери категорию:\nФормат ввода:\n мука, молоко",
@@ -140,14 +192,35 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=["help"])
-def start_message(message):
+def start_message(message: telebot.types.Message) -> None:
+    """
+    Sends a help message to the user when they request it.
+
+    Args:
+        message (telebot.types.Message): The message with information
+        about the user's input (it also contains the chat id).
+
+    Returns:
+        None. It sends a link to the help instructions.
+    """
     bot.send_message(
-        message.chat.id, "Ознакомьтесь с инструкцией: https://clck.ru/3E5g8X"
+        message.chat.id, "Ознакомьтесь с инструкцией: "
     )
 
 
 @bot.message_handler(commands=['mode'])
-def switch_mode(message):
+def switch_mode(message: telebot.types.Message)-> None:
+    """
+    Switches the current mode of the bot between recipe search and question answering.
+
+    Args:
+        message (telebot.types.Message): The message with information
+        about the user's input (it also contains the chat id).
+
+    Returns:
+        None. It changes the current_mode and sends a message to the user
+        confirming the new mode.
+    """
     global current_mode
     if current_mode == MODE_RECIPES:
         current_mode = MODE_QUESTIONS
@@ -159,7 +232,17 @@ def switch_mode(message):
 
 
 @bot.message_handler(func=lambda message: current_mode == MODE_RECIPES)
-def send_recipes(message):
+def send_recipes(message: telebot.types.Message) -> None:
+    """
+    Handles incoming messages when the bot is in recipe search mode.
+
+    Args:
+        message (telebot.types.Message): The message with information
+        about the user's input (it also contains the chat id).
+
+    Returns:
+        None. It sends recipe information to the user based on their input.
+    """
     user_input = message.text
     if user_input in [
         "Горячее",
@@ -181,7 +264,20 @@ def send_recipes(message):
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
+def callback_query(call: telebot.types.CallbackQuery) -> None:
+    """
+    Handles callback queries from the user, specifically for changing the page
+    of recipe results.
+
+    Args:
+        call (telebot.types.CallbackQuery): The object containing information
+        about the user's action, including the data associated with
+        the callback and the user's chat ID.
+
+    Returns:
+        None. It modifies the user_states and sends updated recipe information
+        based on their navigation request.
+    """
     if call.data.startswith("next_"):
         chat_id = int(call.data.split("_")[1])
         user_states[chat_id]["page"] += 1
@@ -190,7 +286,18 @@ def callback_query(call):
 
 
 # Функция для получения информации о вопросе из кэша или API
-def get_question_from_cache(question):
+def get_question_from_cache(question: str) -> dict|None:
+    """
+    Retrieves the cached answer for a given question.
+
+    Args:
+        question (str): The question (user input) for which the cached answer
+        is to be retrieved.
+
+    Returns:
+        The deserialized answer as a dictionary or None if no answer is found
+        or if an error occurs during retrieval.
+    """
     try:
         cached_info = r.get(question)
         if cached_info:
@@ -205,7 +312,17 @@ def get_question_from_cache(question):
 
 
 @bot.message_handler(func=lambda message: current_mode == MODE_QUESTIONS)
-def handle_questions(message):
+def handle_questions(message: telebot.types.Message) -> None:
+    """
+    Handles incoming messages(questions) when the bot is in QUESTION mode.
+
+    Args:
+        message (): The incoming message object containing the user's question
+
+    Returns:
+        None. The function sends a message with an answer
+        back to the user
+    """
     question = message.text
     answer_info = get_question_from_cache(question)
     print(answer_info)
@@ -219,12 +336,12 @@ def handle_questions(message):
             question_answer = model_pipeline(question=question, context=CONTEXT)['answer']
             # Сохраняем ответ в кэш
             r.setex(question, 3600, json.dumps(question_answer))  # Сохраняем как строку JSON
-            # logging.info(f"Ответ сохранен в кэше для вопроса: {question}")
+            logging.info(f"Ответ сохранен в кэше для вопроса: {question}")
             # Отправляем ответ пользователю
             bot.send_message(message.chat.id, question_answer)
             print(question_answer)
         except Exception as e:
-            # logging.error(f"Ошибка при получении ответа от модели: {e}")
+            logging.error(f"Ошибка при получении ответа от модели: {e}")
             bot.send_message(message.chat.id, "Извините, произошла ошибка при обработке вашего запроса.")
 
 
